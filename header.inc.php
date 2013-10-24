0c26445ce24ce477c9bd5ca45cc83aede7afcde1
Lucas Will <lwill@cws.net>
2013-10-24 14:03:03 -0500

    Updated to use correct information for address info - no longer using crm_accounts table

--- header.inc.php
+++ header.inc.php
@@ -12,6 +12,7 @@ $pdf->SetFont('helvetica', 'B', 12);
     $total_count = mysql_fetch_array($total_count_result);
     $count = $total_count['Count'];
 
+    /*
     $account_name_query = "SELECT name, client_id
                          FROM crm_accounts
                          WHERE number = '$account_number'";
@@ -32,10 +33,28 @@ $pdf->SetFont('helvetica', 'B', 12);
     $address1 = $address_info['address1'];
     $address2 = $address_info['address2'];
     $address3 = $address_info['address3'];
+*/
+    $account_name_query = "SELECT name, id
+                         FROM clients
+                         WHERE number = 'C$account_number'";
+    $client_name_result = mysql_query($account_name_query, $profile_dev_db);
+    $client_name_array = mysql_fetch_array($client_name_result);
+    $client_name = $client_name_array['name'];
+    $client_id = $client_name_array['id'];
 
-    $city = $address_info['city'];
-    $state = $address_info['state'];
-    $postal_code = $address_info['postal_code'];
+    $client_address_id_query = "select client_address_id from client_addresses_clients where client_id = '$client_id'";
+    $client_address_id_result = mysql_query($client_address_id_query, $profile_dev_db);
+    $client_address_id_array = mysql_fetch_array($client_address_id_result);
+    
+    $client_address_info_query = "select * from client_addresses where id = '".$client_address_id_array['client_address_id']."'";
+    $client_address_info_result = mysql_query($client_address_info_query, $profile_dev_db);
+    $client_address_info = mysql_fetch_array($client_address_info_result);
+    
+    $address1 = $client_address_info['street'];
+    $address2 = $client_address_info['street2'];
+    $city = $client_address_info['city'];
+    $state = $client_address_info['state'];
+    $postal_code = $client_address_info['postal_code'];
 
     $pdf->Cell(500, 14, "$client_name", '', 0, 'L', 0, 0, 1, 0, '', 'C');
     $pdf->ln(14);
